6d34d9515d4118735da2b765af5a257f5edfe820
evgen1000end <evgen1000end@gmail.com>
2015-07-13 14:46:35 +0300

    Fixed plugins version

--- src/main/java/ru/demkin/gd/MainApp.java
+++ src/main/java/ru/demkin/gd/MainApp.java
@@ -19,6 +19,8 @@ public class MainApp extends Application {
         Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
         stage.setTitle("JavaFX and Maven");
         stage.setScene(new Scene(root));
+
+
         stage.show();
     }
 }
