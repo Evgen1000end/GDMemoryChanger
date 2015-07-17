0438b1ae94beeccdfd16542b44b02e245d285e40
evgen1000end <evgen1000end@gmail.com>
2015-07-17 15:41:59 +0300

    Работа с памятью в отдельном классе

--- src/main/java/ru/demkin/gd/MainApp.java
+++ src/main/java/ru/demkin/gd/MainApp.java
@@ -6,14 +6,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+import ru.demkin.gd.utils.Kernel32;
+import ru.demkin.gd.utils.MemoryUtils;
+import ru.demkin.gd.utils.User32;
 
+import java.nio.ByteBuffer;
 
 
 public class MainApp extends Application {
 
+    final static long baseAddress = 0x002291E0;
+    final static long[] offsets = new long[]{0x68, 0x34C, 0xA74};
+
+
     public static void main(String[] args) throws Exception {
-        launch(args);
+        //launch(args);
+
+        MemoryUtils.ChangeValue("Grim Dawn", baseAddress, offsets, new byte[]{0x05,0x00,0x00,0x00});
     }
+
 
     @Override
     public void start(Stage stage) throws Exception {
---
+++ src/main/java/ru/demkin/gd/utils/MemoryUtils.java
@@ -0,0 +1,124 @@
+package ru.demkin.gd.utils;
+
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+
+/**
+ * Created by evgen1000end on 17.07.2015.
+ */
+public  class MemoryUtils  {
+
+    static Kernel32 kernel32 = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
+    static User32 user32 = (User32) Native.loadLibrary("user32", User32.class);
+
+    public static int PROCESS_VM_READ= 0x0010;
+    public static int PROCESS_VM_WRITE = 0x0020;
+    public static int PROCESS_VM_OPERATION = 0x0008;
+
+
+    static {
+
+    }
+
+    public static Pointer openProcess(int permissions, int pid) {
+        Pointer process = kernel32.OpenProcess(permissions, true, pid);
+        return process;
+    }
+
+    public static byte[] longToBytes(long l) {
+        byte[] result = new byte[8];
+        for (int i = 7; i >= 0; i--) {
+            result[i] = (byte)(l & 0xFF);
+            l >>= 8;
+        }
+        return result;
+    }
+
+    public static long bytesToLong(byte[] b) {
+        long result = 0;
+        for (int i = 0; i < 8; i++) {
+            result <<= 8;
+            result |= (b[i] & 0xFF);
+        }
+        return result;
+    }
+
+    public static int getProcessId(String window) {
+        IntByReference pid = new IntByReference(0);
+        user32.GetWindowThreadProcessId(user32.FindWindowA(null, window), pid);
+
+        return pid.getValue();
+    }
+
+
+
+    public static long findDynAddress(Pointer process, long[] offsets, long baseAddress)
+    {
+
+        long pointer = baseAddress;
+
+        int size = 4;
+        Memory pTemp = new Memory(size);
+        long pointerAddress = 0;
+
+        for(int i = 0; i < offsets.length; i++)
+        {
+            if(i == 0)
+            {
+                kernel32.ReadProcessMemory(process, pointer, pTemp, size, null);
+            }
+
+            pointerAddress = ((pTemp.getInt(0)+offsets[i]));
+
+            if(i != offsets.length-1)
+                kernel32.ReadProcessMemory(process, pointerAddress, pTemp, size, null);
+
+
+        }
+
+        return pointerAddress;
+    }
+
+    public static Memory readMemory(Pointer process, long address, int bytesToRead) {
+        IntByReference read = new IntByReference(0);
+        Memory output = new Memory(bytesToRead);
+
+        kernel32.ReadProcessMemory(process, address, output, bytesToRead, read);
+        return output;
+    }
+
+    public static void writeMemory(Pointer process, long address, byte[] data)
+    {
+        int size = data.length;
+        Memory toWrite = new Memory(size);
+
+        for(int i = 0; i < size; i++)
+        {
+            toWrite.setByte(i, data[i]);
+        }
+
+        boolean b = kernel32.WriteProcessMemory(process, address, toWrite, size, null);
+    }
+
+
+    public static void ChangeValue(String processName, long baseAddress, long[] offsets, byte[] newValue){
+        int pid = getProcessId(processName);
+
+        Pointer process = openProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, pid);
+
+        long dynAddress = findDynAddress(process, offsets, baseAddress);
+
+
+        final long tempAdress =0x1D7369C8;
+        // Memory scoreMem = readMemory(process,dynAddress,4);
+        Memory scoreMem = readMemory(process,tempAdress,4);
+
+        int score = scoreMem.getInt(0);
+
+        writeMemory(process, tempAdress, newValue);
+    }
+
+
+}
